0bca1cd410967ca062c054edf31ac30237bd686e
Gabriel Tellaroli Ramos <bi.tellaroli1@hotmail.com>
2021-11-30 14:37:29 -0300

    fix(peguinDatalayerContentScript.js): :bug: Adicionei um tratamento de erro no penguinDataLayercontentScript.js
    Adicionei um tratamento de erro no penguinDataLayercontentScript.js, caso de algum erro na função postMessage, ele vai acionar o bloco catch() e imprimir o erro no console da página.

--- src/js/penguinDatalayerContentScript.js
+++ src/js/penguinDatalayerContentScript.js
@@ -1,25 +1,33 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.message == 'inject_penguindatalayer_script') {
         var script = document.createElement('script');
         script.innerHTML = ` 
-      if (!window["${request.datalayer}"].push_c) {
-        window["${request.datalayer}"].forEach(elem=>{
-          window.postMessage({
-            dataLayer:"dispatch_datalayer_object_from_window",
-            datalayer_object:JSON.parse(JSON.stringify(elem)),
-            url: window.location.href
-          },"*");
-        });
-        window["${request.datalayer}"].push_c = window["${request.datalayer}"].push;
-        window["${request.datalayer}"].push = function (obj) {
-          window["${request.datalayer}"].push_c(obj);
-          window.postMessage({
-            dataLayer:"dispatch_datalayer_object_from_window",
-            datalayer_object:obj,
-            url: window.location.href
-          },"*");
-        }
-    };`;
+        if (!window["${request.datalayer}"].push_c) {
+            window["${request.datalayer}"].forEach((elem, index, array) => {
+                try {
+                    window.postMessage({
+                        dataLayer: "dispatch_datalayer_object_from_window",
+                        datalayer_object: JSON.parse(JSON.stringify(elem)),
+                        url: window.location.href
+                    }, "*");
+                } catch (error) {
+                    console.error(\`PenguinDataLayer
+ERROR, event not validated: \${array[index].event}
+Event index: \${index}
+Error: \${error}
+                    \`)
+                }
+            });
+            window["${request.datalayer}"].push_c = window["${request.datalayer}"].push;
+            window["${request.datalayer}"].push = function (obj) {
+                window["${request.datalayer}"].push_c(obj);
+                window.postMessage({
+                    dataLayer: "dispatch_datalayer_object_from_window",
+                    datalayer_object: obj,
+                    url: window.location.href
+                }, "*");
+            }
+        };`;
         document.head.appendChild(script);
     }
     sendResponse({
@@ -27,7 +35,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     });
 });
 
-window.addEventListener('message', function(event) {
+window.addEventListener('message', function (event) {
     if (
         event.data.dataLayer &&
         event.data.dataLayer == 'dispatch_datalayer_object_from_window'
@@ -38,7 +46,7 @@ window.addEventListener('message', function(event) {
                 datalayer_object: event.data.datalayer_object,
                 url: event.data.url,
             },
-            function(response) {}
+            function (response) { }
         );
     }
 });
