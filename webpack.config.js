d43101fa7608726dc7bf914c6ba3279ae7f528c3
mnunes <matheusnunesinfo@gmail.com>
2022-09-19 15:11:43 -0300

    first config

--- webpack.config.js
+++ webpack.config.js
@@ -1,197 +1,211 @@
 let webpack = require('webpack'),
-    path = require('path'),
-    fileSystem = require('fs-extra'),
-    env = require('./utils/env'),
-    CopyWebpackPlugin = require('copy-webpack-plugin'),
-    HtmlWebpackPlugin = require('html-webpack-plugin'),
-    TerserPlugin = require('terser-webpack-plugin');
+  path = require('path'),
+  fileSystem = require('fs-extra'),
+  env = require('./utils/env'),
+  CopyWebpackPlugin = require('copy-webpack-plugin'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  TerserPlugin = require('terser-webpack-plugin');
 let { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const ASSET_PATH = process.env.ASSET_PATH || '/';
 
 let alias = {
-    'react-dom': '@hot-loader/react-dom',
+  'react-dom': '@hot-loader/react-dom',
 };
 
 // load the secrets
 let secretsPath = path.join(__dirname, 'secrets.' + env.NODE_ENV + '.js');
 
 let fileExtensions = [
-    'jpg',
-    'jpeg',
-    'png',
-    'gif',
-    'eot',
-    'otf',
-    'svg',
-    'ttf',
-    'woff',
-    'woff2',
+  'jpg',
+  'jpeg',
+  'png',
+  'gif',
+  'eot',
+  'otf',
+  'svg',
+  'ttf',
+  'woff',
+  'woff2',
 ];
 
 if (fileSystem.existsSync(secretsPath)) {
-    alias['secrets'] = secretsPath;
+  alias['secrets'] = secretsPath;
 }
 
 let options = {
-    mode: process.env.NODE_ENV || 'development',
-    entry: {
-        popup: path.join(__dirname, 'src', 'pages', 'Popup', 'index.jsx'),
-        devtools: path.join(__dirname, 'src', 'pages', 'Devtools', 'index.js'),
-        panel: path.join(__dirname, 'src', 'pages', 'Panel', 'index.jsx'),
-        sandboxPenguinValidation: path.join(__dirname, 'src', 'pages', 'Panel', 'sandbox', 'sandbox-penguinValidation.ts'),
-    },
-    chromeExtensionBoilerplate: {
-        notHotReload: ['devtools'],
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'build'),
-        clean: true,
-        publicPath: ASSET_PATH,
-    },
-    module: {
-        rules: [
-            {
-                // look for .css or .scss files
-                test: /\.(css|scss)$/,
-                // in the `src` directory
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                        },
-                    },
-                ],
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    popup: path.join(__dirname, 'src', 'pages', 'Popup', 'index.jsx'),
+    devtools: path.join(__dirname, 'src', 'pages', 'Devtools', 'index.js'),
+    panel: path.join(__dirname, 'src', 'pages', 'Panel', 'index.jsx'),
+    sandboxPenguinValidation: path.join(
+      __dirname,
+      'src',
+      'pages',
+      'Panel',
+      'sandbox',
+      'sandbox-penguinValidation.ts'
+    ),
+  },
+  chromeExtensionBoilerplate: {
+    notHotReload: ['devtools'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+    publicPath: ASSET_PATH,
+  },
+  module: {
+    rules: [
+      {
+        // look for .css or .scss files
+        test: /\.(css|scss)$/,
+        // in the `src` directory
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
             },
-            {
-                test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
-                // type: 'asset/resource',
-                exclude: /node_modules/,
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]',
-                },
-            },
-            {
-                test: /\.html$/,
-                loader: 'html-loader',
-                exclude: /node_modules/,
-            },
-            { test: /\.(ts|tsx)$/, loader: 'ts-loader', exclude: /node_modules/ },
-            {
-                test: /\.(js|jsx)$/,
-                use: [
-                    {
-                        loader: 'source-map-loader',
-                    },
-                    {
-                        loader: 'babel-loader',
-                    },
-                ],
-                exclude: /node_modules/,
-            },
+          },
         ],
-    },
-    resolve: {
-        alias: alias,
-        extensions: fileExtensions
-            .map((extension) => '.' + extension)
-            .concat(['.js', '.jsx', '.ts', '.tsx', '.css']),
-    },
-    plugins: [
-        new CleanWebpackPlugin({ verbose: false }),
-        new webpack.ProgressPlugin(),
-        // expose and write the allowed env vars on the compiled bundle
-        new webpack.EnvironmentPlugin(['NODE_ENV']),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: 'src/manifest.json',
-                    to: path.join(__dirname, 'build'),
-                    force: true,
-                    transform: function (content, path) {
-                        // generates the manifest file using the package.json informations
-                        return Buffer.from(
-                            JSON.stringify({
-                                description: process.env.npm_package_description,
-                                version: process.env.npm_package_version,
-                                ...JSON.parse(content.toString()),
-                            })
-                        );
-                    },
-                },
-            ],
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: 'src/assets/img/icon-128.png',
-                    to: path.join(__dirname, 'build'),
-                    force: true,
-                },
-            ],
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: 'src/assets/img/icon-34.png',
-                    to: path.join(__dirname, 'build'),
-                    force: true,
-                },
-            ],
-        }),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'pages', 'Popup', 'index.html'),
-            filename: 'popup.html',
-            chunks: ['popup'],
-            cache: false,
-        }),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'pages', 'Devtools', 'index.html'),
-            filename: 'devtools.html',
-            chunks: ['devtools'],
-            cache: false,
-        }),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'pages', 'Panel', 'index.html'),
-            filename: 'panel.html',
-            chunks: ['panel'],
-            cache: false,
-        }),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'pages', 'Panel', 'sandbox', 'sandbox.html'),
-            filename: 'sandbox.html',
-            chunks: ['sandboxPenguinValidation'],
-            cache: false,
-        }),
-        new webpack.ProvidePlugin({
-            process: 'process/browser',
-        }),
+      },
+      {
+        test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
+        // type: 'asset/resource',
+        exclude: /node_modules/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        exclude: /node_modules/,
+      },
+      { test: /\.(ts|tsx)$/, loader: 'ts-loader', exclude: /node_modules/ },
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: 'source-map-loader',
+          },
+          {
+            loader: 'babel-loader',
+          },
+        ],
+        exclude: /node_modules/,
+      },
     ],
-    infrastructureLogging: {
-        level: 'info',
-    },
+  },
+  resolve: {
+    alias: alias,
+    extensions: fileExtensions
+      .map((extension) => '.' + extension)
+      .concat(['.js', '.jsx', '.ts', '.tsx', '.css']),
+  },
+  plugins: [
+    new CleanWebpackPlugin({ verbose: false }),
+    new webpack.ProgressPlugin(),
+    // expose and write the allowed env vars on the compiled bundle
+    new webpack.EnvironmentPlugin(['NODE_ENV']),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/manifest.json',
+          to: path.join(__dirname, 'build'),
+          force: true,
+          transform: function (content, path) {
+            // generates the manifest file using the package.json informations
+            return Buffer.from(
+              JSON.stringify({
+                description: process.env.npm_package_description,
+                version: process.env.npm_package_version,
+                ...JSON.parse(content.toString()),
+              })
+            );
+          },
+        },
+      ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/assets/img/icon-128.png',
+          to: path.join(__dirname, 'build'),
+          force: true,
+        },
+      ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/assets/img/icon-34.png',
+          to: path.join(__dirname, 'build'),
+          force: true,
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'pages', 'Popup', 'index.html'),
+      filename: 'popup.html',
+      chunks: ['popup'],
+      cache: false,
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'pages', 'Devtools', 'index.html'),
+      filename: 'devtools.html',
+      chunks: ['devtools'],
+      cache: false,
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'pages', 'Panel', 'index.html'),
+      filename: 'panel.html',
+      chunks: ['panel'],
+      cache: false,
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(
+        __dirname,
+        'src',
+        'pages',
+        'Panel',
+        'sandbox',
+        'sandbox.html'
+      ),
+      filename: 'sandbox.html',
+      chunks: ['sandboxPenguinValidation'],
+      cache: false,
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
+  infrastructureLogging: {
+    level: 'info',
+  },
 };
 
 if (env.NODE_ENV === 'development') {
-    options.devtool = 'cheap-module-source-map';
+  options.devtool = 'cheap-module-source-map';
 } else {
-    options.optimization = {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                extractComments: false,
-            }),
-        ],
-    };
+  options.optimization = {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  };
 }
 
 module.exports = options;
